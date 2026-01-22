4837e7dd6c621035a7fe2e143cdf06fd30e2b061
Mac OS <macos@MacBook-Air-de-Mac.local>
2026-01-22 15:33:12 -0300

    Remove base path for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/AppFinanceiro/",
   server: {
     host: "::",
     port: 8080,
